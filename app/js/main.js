e28d587c6f75acddef08be848b83038eb712d484
Jon Juliusson <Jon Juliusson>
2016-02-19 21:02:28 +0000

    main, home, room

--- app/js/main.js
+++ app/js/main.js
@@ -1,47 +1,60 @@
 angular.module("ChatApp", ["ng", "ngRoute"])
-.config(function($routeProvider, $locationProvider){
+  .config(function($routeProvider, $locationProvider) {
 
-  $routeProvider.when("/index",{
+    $routeProvider.when("/index", {
       templateUrl: "app/views/home.html",
       controller: "HomeCtrl"
-  }).when("/rooms/:roomId",{
-    templateUrl: "app/views/room.html",
-    controller: "RoomCtrl"
-  }).otherwise({ redirectTo: "/index"});
+    }).when("/rooms/:roomId", {
+      templateUrl: "app/views/room.html",
+      controller: "RoomCtrl"
+    }).otherwise({
+      redirectTo: "/index"
+    });
 
-});
-
-angular.module("ChatApp").controller("HomeCtrl",["$scope", "$http",
-function($scope,$http){
-  $scope.loggedIn = false;
-  var socket = io.connect("http://localhost:8080");
-  socket.on("roomlist", function(data){
-      console.log(data);
   });
 
-  $scope.nick = "";
-  $scope.login = function(){
-      socket.emit("adduser", $scope.nick, function(available){
+angular.module("ChatApp").controller("HomeCtrl", ["$scope", "$http",
+  function($scope, $http) {
+    $scope.loggedIn = false;
+    var socket = io.connect("http://localhost:8080");
+    socket.on("roomlist", function(data) {
+      $scope.rooms = data;
+      $scope.$apply();
+      console.log(data);
+    });
+    $scope.channels = 0;
+
+    $scope.selectedRoom = "";
+
+    $scope.nick = "";
+    $scope.login = function() {
+      socket.emit("adduser", $scope.nick, function(available) {
         console.log(available)
-      if (available){
+        if (available) {
           // The "dabs" username is not taken!
           $scope.loggedIn = true;
           socket.emit("rooms");
-      }
-    });
-  };
+        }
+      });
+    };
 
-  $scope.joinroom = function(){
-      socket.emit("joinroom", {room: $scope.room, pass: $scope.pass }, function(canIjoin){
+      $scope.joinroom = function() {
+      socket.emit("joinroom", {
+        room: $scope.room,
+        pass: $scope.pass
+      }, function(canIjoin) {
         console.log("canIjoin: " + canIjoin);
         socket.emit("rooms");
+        if(canIjoin){
+          $scope.channels += 1;
+        }
       });
-  };
+    };
 
-}]);
+  }
+]);
 
-angular.module("ChatApp").controller("RoomCtrl",
-["$scope", "$http",
-function($scope, $http){
+angular.module("ChatApp").controller("RoomCtrl", ["$scope", "$http",
+function($scope, $http) {
 
 }]);
